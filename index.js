ebbcc5707904ba550c52240ab7a30e1f56d2091f
David Alen <davidalen51@gmail.com>
2020-05-16 13:42:20 -0300

    [fix] file issues on index.js

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const { get, post } = require('./src/upload');
+const bp = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+
+// upload routes
+const { get, post } = require('./src/upload/upload');
 
 const app = express();
 
-const port = 8080;
+// enable files upload
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
+
+// add other middleware
+app.use(cors());
+app.use(bp.json());
+
+app.use(bp.urlencoded({ extended: true }));
+
+// port
+const port = process.env.PORT || 8080;
 
 app.get('/upload', get);
 app.post('/upload', post);
