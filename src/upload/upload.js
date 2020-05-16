f12192b778b2480272ab68f554cda805e4920028
David Alen <davidalen51@gmail.com>
2020-05-16 13:41:48 -0300

    [add] upload files

--- src/upload/upload.js
+++ src/upload/upload.js
@@ -1,3 +1,5 @@
+const { FILES_IS_UNDEFINED_ERROR, FORBIDDEN_UPLOAD_ERROR } = require('../settings/default_messages');
+
 const get = (req, res) => {
   res.status(200).json({ message: 'Hello, from uploads!' });
   return 0;
@@ -5,6 +7,17 @@ const get = (req, res) => {
 
 const post = (req, res) => {
   // TODO: Get file from express and store it in cache for next microservice to work
+
+  if (!req.headers.email || !req.headers.user_key) {
+    res.status(FORBIDDEN_UPLOAD_ERROR.status).json(FORBIDDEN_UPLOAD_
+  }
+
+  if (req.files == undefined) {
+    res.status(FILES_IS_UNDEFINED_ERROR.status).json(FILES_IS_UNDEFINED_ERROR);
+    return;
+  }
+
+  res.status(200).json({ message: 'OK' });
 };
 
 module.exports = {
